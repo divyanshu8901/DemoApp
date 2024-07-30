ca880974620f64820fb7b066a96abac6711dd896
divyanshu8901 <divyanshuson@gmail.com>
2024-07-30 21:55:15 +0530

    UserDefinedException

---
+++ src/in/ineuron/bean1/LaunchRto.java
@@ -0,0 +1,66 @@
+package in.ineuron.bean1;
+import java.util.Scanner;
+
+class NotValidAgeException extends Exception
+{
+	public NotValidAgeException(String message)
+	{
+		super(message);
+	}
+}
+
+class Applicant
+{
+	 	int age ;
+
+		public void input()
+		{
+			Scanner sc = new Scanner(System.in );
+			System.out.println("Enter your age :");
+			age = sc.nextInt();
+			sc.close();
+		}
+	
+		public void verify()
+		{
+			if(age < 18)
+			{
+				 
+				NotValidAgeException e = new NotValidAgeException("you are not elegible");
+				System.out.println(e.getMessage());
+				
+			}else if (age > 60)
+			{
+				NotValidAgeException e = new NotValidAgeException("you are not elegible");
+				System.out.println(e.getMessage());
+				
+			}else 
+			{
+				System.out.println("you are eligible");
+			}
+		}
+		
+	 
+}
+
+class RTO
+{
+	public void initiate()
+	{
+		Applicant a = new Applicant();
+		a.input();
+		a.verify();
+	}
+	
+}
+
+
+public class LaunchRto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			RTO rt = new RTO();
+			rt.initiate();
+	}
+
+}
